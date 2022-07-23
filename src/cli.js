fd27507057606806069cbdfc6337b81462491b13
igor_briukhovetskii <igor@bruhovet.ru>
2022-07-23 23:54:18 +0300

    fix linter errors

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const sayHello = () => {
-    const userName = readlineSync.question('May I have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
 
-    console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 };
 
 export default sayHello;
